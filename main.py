fa88f8e0dc252f869da80027922998b5408318c0
Noah Bortel <nbortel98@gmail.com>
2025-07-16 05:44:30 -0700

    fix: correct syntax error in defintiion

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main() -> None:
     event_handler = EventHandler()
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc - Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
     with tcod.context.new_terminal(
