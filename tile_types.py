e246929c44d8e2b273f2147a323984cf79a4d1c8
Noah Bortel <nbortel98@gmail.com>
2025-07-16 06:43:17 -0700

    fix: correct misspelled identifiers

--- tile_types.py
+++ tile_types.py
@@ -31,7 +31,7 @@ def new_tile(
 
 
 floor = new_tile(
-    walkable=True, tranparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
 )
 wall = new_tile(
     walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
