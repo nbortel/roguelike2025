e1695be09d92dc836c820aa7f2bc15b20ee08d6d
Noah Bortel <nbortel98@gmail.com>
2025-07-17 08:48:40 -0700

    refactor: tcod pt6 refactor

--- game_map.py
+++ game_map.py
@@ -7,22 +7,34 @@ from tcod.console import Console
 import tile_types
 
 if TYPE_CHECKING:
+    from engine import Engine
     from entity import Entity
 
 
 class GameMap:
-    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
+    def __init__(
+            self, engine: Engine, width: int, height: int, entities: Iterable[Entity] = ()
+    ):
+        self.engine = engine
         self.width, self.height = width, height
         self.entities = set(entities)
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
-        # currently visible tiles
-        self.visible = np.full((width, height), fill_value=False, order="F")
-        # tiles which have been visble in the past
-        self.explored = np.full((width, height), fill_value=False, order="F")
+        self.visible = np.full(
+                (width, height), fill_value=False, order="F"
+        ) # currently visible tiles
+        self.explored = np.full(
+                (width, height), fill_value=False, order="F"
+        ) # tiles which have been visble in the past
 
-    def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+    def get_blocking_entity_at_location(
+            self, location_x: int, location_y: int
+    ) -> Optional[Entity]:
         for entity in self.entities:
-            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+            if (
+                entity.blocks_movement
+                and entity.x == location_x
+                and entity.y == location_y
+            ):
                 return entity
         return None
 
