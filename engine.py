e28db1d6ade52265b0c13ecca11542f858fb5d92
Noah Bortel <nbortel98@gmail.com>
2025-07-19 08:58:51 -0700

    feat: initialize message log

--- engine.py
+++ engine.py
@@ -7,6 +7,7 @@ from tcod.console import Console
 from tcod.map import compute_fov
 
 from input_handlers import MainGameEventHandler
+from message_log import MessageLog
 from render_functions import render_bar
 
 if TYPE_CHECKING:
@@ -20,13 +21,13 @@ class Engine:
 
     def __init__(self, player: Actor):
         self.event_handler: EventHandler = MainGameEventHandler(self)
+        self.message_log = MessageLog()
         self.player = player
 
     def handle_enemy_turns(self) -> None:
         for entity in set(self.game_map.actors) - {self.player}:
             if entity.ai:
                 entity.ai.perform()
-
 
     def update_fov(self) -> None:
         """Recompute the visble area based on the players point of view."""
@@ -39,6 +40,10 @@ class Engine:
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
+
+        self.message_log.render(
+                console=console, x=21, y=45, width=40, height=5
+        )
 
         render_bar(
             console=console,
