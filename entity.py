136d76e99ea4fcbb62ff8deaead4c587bc700c3a
Noah Bortel <nbortel98@gmail.com>
2025-07-21 07:38:55 -0700

    feat: Actors have Inventory component

--- entity.py
+++ entity.py
@@ -9,7 +9,9 @@ if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.consumable import Consumable
     from components.fighter import Fighter
+    from components.inventory import Inventory
     from game_map import GameMap
+
 
 T = TypeVar("T", bound="Entity")
 
@@ -86,7 +88,8 @@ class Actor(Entity):
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
         ai_cls: Type[BaseAI],
-        fighter: Fighter
+        fighter: Fighter,
+        inventory: Inventory,
     ):
         super().__init__(
             x=x,
@@ -102,6 +105,9 @@ class Actor(Entity):
 
         self.fighter = fighter
         self.fighter.parent = self
+
+        self.inventory = inventory
+        self.inventory.parent = self
 
     @property
     def is_alive(self) -> bool:
