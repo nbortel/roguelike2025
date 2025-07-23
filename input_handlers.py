601bbbe3595898adfca4272bf932caabf0913803
Noah Bortel <nbortel98@gmail.com>
2025-07-23 08:58:18 -0700

    feat: add PopupMessage

--- input_handlers.py
+++ input_handlers.py
@@ -85,6 +85,35 @@ class BaseEventHandler(tcod.event.EventDispatch[ActionOrHandler]):
         raise SystemExit()
 
 
+class PopupMessage(BaseEventHandler):
+    """Display a popup text window."""
+
+    def __init__(self, parent_handler: BaseEventHandler, text: str):
+        self.parent = parent_handler
+        self.text = text
+
+    def on_render(self, console: tcod.Console) -> None:
+        """Render the parent and dim the result,
+        then print the message on top."""
+        self.parent.on_render(console)
+        console.tiles_rgb["fg"] //= 8
+        console.tiles_rgb["bg"] //= 8
+
+        console.print(
+                console.width // 2,
+                console.height // 2,
+                self.text,
+                fg=color.white,
+                bg=color.black,
+                alignment=tcod.CENTER,
+        )
+
+    def ev_keydown(
+            self, event: tcod.event.KeyDown) -> Optional[BaseEventHandler]:
+        """ Any key returns to the parent handler."""
+        return self.parent
+
+
 class EventHandler(BaseEventHandler):
     def __init__(self, engine: Engine):
         self.engine = engine
