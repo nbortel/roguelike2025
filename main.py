592816f5f263f4840e66e97600281ab7e4a28b6f
Noah Bortel <nbortel98@gmail.com>
2025-07-27 01:39:32 -0700

    feat: add new font

--- main.py
+++ main.py
@@ -31,9 +31,12 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    tileset = tcod.tileset.load_truetype_font("QuinqueFive.ttf", 20, 16)
+    """ Orignal TCOD tutorial tileset
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+    """
 
     handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
 
